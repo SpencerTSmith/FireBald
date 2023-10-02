a5077899ba852075473ed6c47746289dbec2bdf8
SpencerTSmith <spencertsmith2003@gmail.com>
2023-10-02 15:05:08 -0500

    Implement c++ class for projectiles

---
+++ Source/MyProject/ProjectileActor.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProjectileActor.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+
+// Sets default values
+AProjectileActor::AProjectileActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	if (!RootComponent) 
+	{
+		RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("ProjectileSceneComponent"));
+	}
+	if (!CollisionComponent)
+	{
+		CollisionComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+		RootComponent = CollisionComponent;
+	}
+	if (!ProjectileMovementComponent)
+	{ 
+		ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComponent"));
+		ProjectileMovementComponent->SetUpdatedComponent(CollisionComponent);
+	}
+}
+
+// Called when the game starts or when spawned
+void AProjectileActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AProjectileActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
