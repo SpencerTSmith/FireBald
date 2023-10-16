ba5bdc0003cce3822946d3a8edec2e0643ed7764
SpencerTSmith <spencertsmith2003@gmail.com>
2023-10-15 19:01:23 -0500

    Fix a couple misc things

--- Source/MyProject/MyCharacter.h
+++ Source/MyProject/MyCharacter.h
@@ -114,7 +114,7 @@ private:
 	static constexpr float StunStaminaCost = 10.0f;
 	float MaxStamina = BaseStamina;
 	float CurrentStamina = BaseStamina;
-	float StaminaRestorationFactor = 0.0f;
+	float StaminaRestorationFactor = 0.5f;
 
 	// Fireball Charge
 	static constexpr float MaxFireballCharge = 50.0f;
